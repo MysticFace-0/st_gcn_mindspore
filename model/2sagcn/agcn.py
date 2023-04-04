93d75dc59989c2994cfeba8af6100eb59f44c21f
MysticFace <1290971635@qq.com>
2023-04-04 20:12:10 +0800

    unit_gcn (with conv_branch_init bug)

--- model/2sagcn/agcn.py
+++ model/2sagcn/agcn.py
@@ -48,58 +48,61 @@ class unit_tcn(nn.Cell):
         return x
 
 
-class unit_gcn(nn.Module):
+class unit_gcn(nn.Cell):
     def __init__(self, in_channels, out_channels, A, coff_embedding=4, num_subset=3):
         super(unit_gcn, self).__init__()
         inter_channels = out_channels // coff_embedding
         self.inter_c = inter_channels
-        self.PA = nn.Parameter(torch.from_numpy(A.astype(np.float32)))
-        nn.init.constant_(self.PA, 1e-6)
-        self.A = Variable(torch.from_numpy(A.astype(np.float32)), requires_grad=False)
+        self.PA = Parameter(A, requires_grad=True)
+        constant_init_bias = mindspore.common.initializer.Constant(value=1e-6)
+        constant_init_bias(self.PA)
+        self.A = Parameter(A, requires_grad=False)
         self.num_subset = num_subset
 
-        self.conv_a = nn.ModuleList()
-        self.conv_b = nn.ModuleList()
-        self.conv_d = nn.ModuleList()
+        self.conv_a = nn.CellList()
+        self.conv_b = nn.CellList()
+        self.conv_d = nn.CellList()
         for i in range(self.num_subset):
             self.conv_a.append(nn.Conv2d(in_channels, inter_channels, 1))
             self.conv_b.append(nn.Conv2d(in_channels, inter_channels, 1))
             self.conv_d.append(nn.Conv2d(in_channels, out_channels, 1))
 
         if in_channels != out_channels:
-            self.down = nn.Sequential(
+            self.down = nn.SequentialCell(
                 nn.Conv2d(in_channels, out_channels, 1),
                 nn.BatchNorm2d(out_channels)
             )
         else:
             self.down = lambda x: x
 
+        self.matmul = ops.MatMul()
         self.bn = nn.BatchNorm2d(out_channels)
         self.soft = nn.Softmax(-2)
         self.relu = nn.ReLU()
 
-        for m in self.modules():
+        for m in self.cells():
             if isinstance(m, nn.Conv2d):
                 conv_init(m)
             elif isinstance(m, nn.BatchNorm2d):
                 bn_init(m, 1)
         bn_init(self.bn, 1e-6)
         for i in range(self.num_subset):
+            print(self.conv_d[i])
             conv_branch_init(self.conv_d[i], self.num_subset)
 
-    def forward(self, x):
-        N, C, T, V = x.size()
-        A = self.A.cuda(x.get_device())
+    def construct(self, x):
+        N, C, T, V = x.shape
+        A = self.A
         A = A + self.PA
 
         y = None
         for i in range(self.num_subset):
-            A1 = self.conv_a[i](x).permute(0, 3, 1, 2).contiguous().view(N, V, self.inter_c * T)
+            A1 = self.conv_a[i](x).permute(0, 3, 1, 2).view(N, V, self.inter_c * T)
             A2 = self.conv_b[i](x).view(N, self.inter_c * T, V)
-            A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1))  # N V V
+            A1 = self.soft(self.matmul(A1, A2) / A1.size(-1))  # N V V
             A1 = A1 + A[i]
             A2 = x.view(N, C * T, V)
-            z = self.conv_d[i](torch.matmul(A2, A1).view(N, C, T, V))
+            z = self.conv_d[i](self.matmul(A2, A1).view(N, C, T, V))
             y = z + y if y is not None else z
 
         y = self.bn(y)
@@ -107,7 +110,7 @@ class unit_gcn(nn.Module):
         return self.relu(y)
 
 
-class TCN_GCN_unit(nn.Module):
+class TCN_GCN_unit(nn.Cell):
     def __init__(self, in_channels, out_channels, A, stride=1, residual=True):
         super(TCN_GCN_unit, self).__init__()
         self.gcn1 = unit_gcn(in_channels, out_channels, A)
@@ -122,7 +125,7 @@ class TCN_GCN_unit(nn.Module):
         else:
             self.residual = unit_tcn(in_channels, out_channels, kernel_size=1, stride=stride)
 
-    def forward(self, x):
+    def construct(self, x):
         x = self.tcn1(self.gcn1(x)) + self.residual(x)
         return self.relu(x)
 
@@ -150,7 +153,8 @@ class AGCN(nn.Cell):
         self.l10 = TCN_GCN_unit(256, 256, A)
 
         self.fc = nn.Dense(256, num_class)
-        nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
+        constant_init_weight = mindspore.common.initializer.Normal(sigma=math.sqrt(2. / num_class), mean=0.0)
+        constant_init_weight(self.fc.weight)
         bn_init(self.data_bn, 1)
 
     def construct(self, x):
